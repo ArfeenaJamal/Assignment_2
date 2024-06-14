b2ea5be14917b27bcfa43a5b4a053cc0e3ad67c1
arfeena <ajamal7816>
2024-06-14 07:50:47 +0530

    fuctions ,switch cases

--- Assignment_2/Program.cs
+++ Assignment_2/Program.cs
@@ -1,16 +1,47 @@
 ﻿using System;
-    class position
+    class Place
 {
     int x{ get; set; }
     int y{ get; set; }
 
-    public position(int x,int y)
+    public Place(int x,int y)
         { 
         int X = x;
           int Y=y; 
     }
 
 }
-    class player
-{ }
+class Participants
+{
+    public string Name { get; set; }
+    public Place Place { get; set; }
+    public int Gemcount { get; set; }
+    public Participants(string name, Place place)
+    {
+        Name = name;
+        Place = place;
+        Gemcount = 0;
+    }
+    public void changeplace(char direction)
+    {
+        switch (direction)
+        {
+            case 'U':
+                Place Y--;
+                break;
+            case 'D':
+                Place Y++;
+                break;
+            case 'L':
+                Place X--;
+                break;
+            case 'R':
+                Place X++;
+                break;
+        }
+    }
+
+
+
+}
   
